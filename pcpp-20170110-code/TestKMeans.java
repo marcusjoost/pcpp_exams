b52cd1d80009e495e5a37f10ba299e16a537ff0c
marcus joost <trolljoost@gmail.com>
2018-12-15 10:35:15 +0100

    question 2 in 10/1-2017 exam

--- pcpp-20170110-code/TestKMeans.java
+++ pcpp-20170110-code/TestKMeans.java
@@ -31,7 +31,7 @@ public class TestKMeans {
         final Point[] points = GenerateData.randomPoints(n);
         final int[] initialPoints = GenerateData.randomIndexes(n, k);
         for (int i = 0; i < 3; i++) {
-            timeKMeans(new KMeans1(points, k), initialPoints);
+            timeKMeans(new KMeans2P (points, k), initialPoints);
             // timeKMeans(new KMeans1P(points, k), initialPoints);
             // timeKMeans(new KMeans2(points, k), initialPoints);
             // timeKMeans(new KMeans2P(points, k), initialPoints);
@@ -142,6 +142,8 @@ class KMeans1 implements KMeans {
     }
 }
 
+// -- PARALLEL -------------------
+
 class KMeans1P implements KMeans {
     // Parallel version 1. A Cluster has an immutable mean field, and
     // a mutable list of immutable Points.
@@ -243,7 +245,7 @@ class KMeans1P implements KMeans {
             return mean;
         }
 
-        public void add(Point p) {
+        public synchronized void add(Point p) {
             points.add(p);
         }
 
@@ -294,6 +296,117 @@ class KMeans2 implements KMeans {
                     myCluster[pi] = best;
                 }
             }
+            {
+                // Update step: recompute mean of each cluster
+                for (Cluster c : clusters)
+                    c.resetMean();
+                for (int pi = 0; pi < points.length; pi++)
+                    myCluster[pi].addToMean(points[pi]);
+                converged = true;
+                for (Cluster c : clusters)
+                    converged &= c.computeNewMean();
+            }
+            // System.out.printf("[%d]", iterations); // To diagnose infinite loops
+        }
+        this.clusters = clusters;
+    }
+
+    public void print() {
+        for (Cluster c : clusters)
+            System.out.println(c);
+        System.out.printf("Used %d iterations%n", iterations);
+    }
+
+    static class Cluster extends ClusterBase {
+        private Point mean;
+        private double sumx, sumy;
+        private int count;
+
+        public Cluster(Point mean) {
+            this.mean = mean;
+        }
+
+        public void addToMean(Point p) {
+            sumx += p.x;
+            sumy += p.y;
+            count++;
+        }
+
+        // Recompute mean, return true if it stays almost the same, else false
+        public boolean computeNewMean() {
+            Point oldMean = this.mean;
+            this.mean = new Point(sumx / count, sumy / count);
+            return oldMean.almostEquals(this.mean);
+        }
+
+        public void resetMean() {
+            sumx = sumy = 0.0;
+            count = 0;
+        }
+
+        @Override
+        public Point getMean() {
+            return mean;
+        }
+    }
+}
+
+// -- PARALLEL -------------------
+
+class KMeans2P implements KMeans {
+    // Parallel version 2. Data represention: An array points of
+    // Points and a same-index array myCluster of the Cluster to which
+    // each point belongs, so that points[pi] belongs to myCluster[pi],
+    // for each Point index pi. A Cluster holds a mutable mean field
+    // and has methods for aggregation of its value.
+
+    private final Point[] points;
+    private final int k;
+    private Cluster[] clusters;
+    private int iterations;
+
+    public KMeans2P(Point[] points, int k) {
+        this.points = points;
+        this.k = k;
+    }
+
+    public void findClusters(int[] initialPoints) {
+        final Cluster[] clusters = GenerateData.initialClusters(points, initialPoints, Cluster::new, Cluster[]::new);
+        final Cluster[] myCluster = new Cluster[points.length];
+        int taskCount = 8;
+        int work_per_task_range = points.length/taskCount;
+        ExecutorService executor = Executors.newWorkStealingPool();
+        boolean converged = false;
+        while (!converged) {
+            iterations++;
+            {
+                ArrayList<Future<?>> tasks = new ArrayList<Future<?>>();
+                for(int i = 0; i < taskCount; i++) {
+                    int to = i * work_per_task_range;
+                    int from = (i + 1 == taskCount) ? points.length : work_per_task_range * (i + 1); 
+                    Cluster[] cs = clusters;
+                    Cluster[] mcs = myCluster;
+                    // Assignment step: put each point in exactly one cluster
+                    tasks.add(executor.submit(() -> {
+                        for (int pi = from; pi < to; pi++) {
+                            Point p = points[pi];
+                            Cluster best = null;
+                            for (Cluster c : cs)
+                                if (best == null || p.sqrDist(c.mean) < p.sqrDist(best.mean))
+                                    best = c;
+                            mcs[pi] = best;
+                        }
+                    }));
+                }
+                
+                for(Future task : tasks) {
+                    try {
+                        task.get();
+                    } catch (Exception e){
+
+                    }
+                }
+            }  
             {
                 // Update step: recompute mean of each cluster
                 for (Cluster c : clusters)
